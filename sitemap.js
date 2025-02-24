42d5d423ebf80d6c3624b22b13bddd01d2237a1e
Km-85 <81754044+Km-85@users.noreply.github.com>
2025-02-24 21:02:44 +0530

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -3,16 +3,6 @@ const links = [
   { url: '/about', changefreq: 'weekly', priority: 0.8 },
   { url: '/services', changefreq: 'weekly', priority: 0.8 },
   { url: '/contact', changefreq: 'monthly', priority: 0.6 },
-   { url: '/tools/image-compressor', changefreq: 'weekly', priority: 0.8 },
-   { url: '/tools/image-converter', changefreq: 'weekly', priority: 0.8 },
-{ url: '/tools/thumbnail-downloader', changefreq: 'weekly', priority: 0.8 },
-{ url: '/services/seo-services', changefreq: 'weekly', priority: 0.8 },
-{ url: '/services/web-development', changefreq: 'weekly', priority: 0.8 },
-{ url: '/services/social-media-marketing', changefreq: 'weekly', priority: 0.8 },
-{ url: '/services/ads', changefreq: 'weekly', priority: 0.8 },
-{ url: '/services/email-marketing', changefreq: 'weekly', priority: 0.8 },
- { url: '/services/content-marketing', changefreq: 'weekly', priority: 0.8 }, 
-  
 ];
 
 const stream = new SitemapStream({ hostname: 'https://www.groovstacks.com/' });
